d03fc7b576df07dafb17f4aa3180867294503af8
souvikjs01 <souvik741156@gmail.com>
2025-03-16 10:45:15 +0530

    canvas ignore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.fallback = {
+      canvas: false, // Ignore canvas for client-side
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
